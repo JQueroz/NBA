b32ac6b35fdf89db8891a6b3f29aff68265dda40
lab04 <lab04@lab4-18>
2024-09-23 22:26:39 -0300

    Inserção dos campos para cadastro e suas publicações dentro do codigo.

--- cadastroNBA-main/src/usa/com/nba/atletas/operacoesController.java
+++ cadastroNBA-main/src/usa/com/nba/atletas/operacoesController.java
@@ -1,5 +1,36 @@
 package usa.com.nba.atletas;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+@SuppressWarnings("unused")
 public class operacoesController {
+	
+	@FXML
+	public TextField txfName;
+	
+	@FXML
+	public TextField txfSoname;
+	
+	@FXML
+	public TextField txfIdade;
+	
+	@FXML
+	public TextField txfEquipe;
+	
+	@FXML
+	public TextField txfPosicao;
+	
+	@FXML
+	public Button btnCadastrar;
 	
 }
